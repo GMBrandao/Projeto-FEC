6f8d050a8028e563c57d5979c6cb4e23f5287371
GBrandao04 <106107645+GBrandao04@users.noreply.github.com>
2022-08-23 09:40:49 -0300

    Versão 1.0 para testes

---
+++ Site-FEC/src/JS/BarraStats.js
@@ -0,0 +1,10 @@
+//Barra de atributos abre e fecha
+const BarraStatus = document.querySelector('.stats-detalhado');
+
+function abrirStats() {
+    BarraStatus.style.display = 'grid';
+}
+function fecharStats() {
+    BarraStatus.style.display = 'none';
+}
+//Fim barra de atributos
